8bc2dec591c2744f13f64bc676b9f4dda7c40f6c
Yasuaki K <user@USER-no-MacBook-Pro.local>
2017-02-09 02:02:06 +0900

    chap4 end

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,65 @@
+class ListingsController < ApplicationController
+before_action :authenticate_user!
+  before_action :set_listing, only: [:update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
+
+
+  def index
+  end
+
+  def new
+    @listing = current_user.listings.new
+    # DBへの保存はしていない。
+  end
+
+  def create
+    @listing = current_user.listings.build(listing_params)
+
+    if @listing.save
+      redirect_to manage_listing_basics_path(current_user), notice:"保存できました。"
+  else
+      redirect_to liting_path, notice:"保存できませんでした。"
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @listing.update(listing_params)
+      redirect_to :back, notice:"更新できました。"
+    end
+  end
+
+  def basics
+  end
+
+  def description
+  end
+
+  def address
+  end
+
+  def price
+  end
+
+  def photos
+  end
+
+  def calendar
+  end
+
+  def bankaccount
+  end
+
+  def publish
+  end
+
+  private
+    def listing_params
+      params.require(:listing).permit(:home_type, :pet_type, :breeding_years, :pet_size, :price_pernight, :address)
+    end
+
+    def set_listing
+      @listing = Listing.find(params[:id])
+    end
+end
